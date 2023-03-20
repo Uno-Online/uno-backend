6a1cdcaf78a94227f3a44a85dd7371ca5c7a28c8
drelocatelli <andressalocatelli@hotmail.com>
2023-03-20 09:56:10 -0300

    setup swagger

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,24 @@ import express from 'express';
 import http from 'http';
 import { SocketServer } from './socket-server';
 import { router } from './routes';
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
 
 const app = express();
+const server = http.createServer(app);
 
 app.use(express.json());
 app.use('/', express.static('public'));
 app.use(router);
 
-const server = http.createServer(app);
+const swaggerDocument = JSON.parse(fs.readFileSync('./swagger.json', 'utf-8'));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 SocketServer.init(server);
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
-    // eslint-disable-next-line no-console
     console.log(`
         Running at: http://localhost:${port}
         Documentation: http://localhost:${port}/docs
