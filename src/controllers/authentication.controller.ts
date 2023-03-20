44173b52aa3ed751bfcc0337755d313464119e61
Hélio Kröger <small.boat6245@heliokroger.com>
2023-03-20 22:35:13 +0000

    feat: adding schema migration to docker-compose

--- src/controllers/authentication.controller.ts
+++ src/controllers/authentication.controller.ts
@@ -1,23 +1,17 @@
-import type { Response } from 'express';
+import type { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
 import { Prisma } from '@prisma/client';
-
 import { prisma } from '../prisma';
-import regiterBodyDto, { RegisterBody } from '../dtos/RegisterBodyDTO';
-import type { Controller } from '../types';
-import { TypedBodyRequest } from '../types/typedBody';
-import { JWTAdapter } from '../adapters/jwt_adapter';
+import { JWTAdapter } from '../adapters/jwt-adapter';
 import { SALT_ROUNDS } from '../constants';
+import { registerUserValidationSchema } from '../validation-schemas/authentication.schema';
 
-export class AuthenticationController implements Controller {
-  private jwt = new JWTAdapter(process.env!.JWT_SECRET!);
+export class AuthenticationController {
+  private jwt = new JWTAdapter(process.env.JWT_SECRET);
 
-  async index() {
-    throw new Error();
-  }
+  async register(req: Request, res: Response) {
+    const body = registerUserValidationSchema.safeParse(req.body);
 
-  async register(req: TypedBodyRequest<RegisterBody>, res: Response) {
-    const body = regiterBodyDto.safeParse(req.body);
     if (!body.success) {
       res.status(400).send('Invalid register request body');
       return;
--- src/types/typedBody.ts
+++
@@ -1,5 +0,0 @@
-import type { Request } from 'express';
-
-export interface TypedBodyRequest<T> extends Request {
-  body: T;
-}
