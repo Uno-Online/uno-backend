411dd85af1f4653ba6c6d955340deb555ecb51f6
Alan Henrique <alancamargo50@gmail.com>
2023-03-23 12:17:01 -0300

    Yarn format

--- src/routes/user/get-user-by-id.ts
+++ src/routes/user/get-user-by-id.ts
@@ -6,19 +6,19 @@ import { prisma } from '../../prisma';
  * Retorna informações buscadas de um usuário
  * */
 export const getUserById = async (req: RequestWithUser, res: Response) => {
-  const { id } = req.params
+  const { id } = req.params;
 
   const userFound = await prisma.user.findUnique({
     where: {
-      id: Number(id)
+      id: Number(id),
     },
     select: {
       id: true,
-      username: true
-    }
-  })
+      username: true,
+    },
+  });
 
-  if(!userFound) {
+  if (!userFound) {
     return res.status(400).json({ success: false, message: 'User not found' });
   }
 
