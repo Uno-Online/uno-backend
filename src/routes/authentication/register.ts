3b5621b5b15149620ef8375d3cf7890da610bc71
Kaolhou <xdroid42@gmail.com>
2023-03-23 11:07:05 -0300

    prettier

--- src/routes/authentication/register.ts
+++ src/routes/authentication/register.ts
@@ -22,8 +22,9 @@ export const register = async (req: Request, res: Response) => {
       data: {
         username: data.username,
         email: data.email,
-        passwordHash: data.password && await bcrypt.hash(data.password, SALT_ROUNDS),
-        isGuest: !!(data.password&&data.email), // todo isso me parece pouco seguro
+        passwordHash:
+          data.password && (await bcrypt.hash(data.password, SALT_ROUNDS)),
+        isGuest: !!(data.password && data.email), // todo isso me parece pouco seguro
       },
     });
 
