dcca1cb621f380b41ee7c54176944ef2ed423f9d
Alan Henrique <alancamargo50@gmail.com>
2023-03-23 11:47:08 -0300

    Removido validação usuário

--- src/routes/profile/get-user-logged.ts
+++ src/routes/profile/get-user-logged.ts
@@ -5,14 +5,10 @@ import { RequestWithUser } from '../../types/request-with-user';
  * Retorna informações sobre o usuário logado
  * */
 export const getUserLogged = async (req: RequestWithUser, res: Response) => {
-  if (!req.user) {
-    return res.json({ success: false, message: 'user not logged' });
-  }
-
   const userLogged = {
-    id: req.user.id,
-    username: req.user.username,
-    email: req.user.email,
+    id: req.user?.id,
+    username: req.user?.username,
+    email: req.user?.email,
   };
 
   return res.json(userLogged);
