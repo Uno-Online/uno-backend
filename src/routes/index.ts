4b3d357a57f1a12bd7d08985617333e19eeaf279
Alan Henrique <alancamargo50@gmail.com>
2023-03-23 12:14:42 -0300

    Adicionado rota para retornar informações de um usuário pelo id

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,11 @@ import { Router } from 'express';
 import { router as roomsRouter } from './rooms';
 import { router as authRouter } from './authentication';
 import { router as profileRouter } from './profile';
+import { router as userRouter } from './user';
 
 export const router = Router();
 
 router.use('/rooms', roomsRouter);
 router.use('/authentication', authRouter);
 router.use('/profile', profileRouter);
+router.use('/users', userRouter);
---
+++ src/routes/user/get-user-by-id.ts
@@ -0,0 +1,26 @@
+import type { Response } from 'express';
+import { RequestWithUser } from '../../types/request-with-user';
+import { prisma } from '../../prisma';
+
+/**
+ * Retorna informações buscadas de um usuário
+ * */
+export const getUserById = async (req: RequestWithUser, res: Response) => {
+  const { id } = req.params
+
+  const userFound = await prisma.user.findUnique({
+    where: {
+      id: Number(id)
+    },
+    select: {
+      id: true,
+      username: true
+    }
+  })
+
+  if(!userFound) {
+    return res.status(400).json({ success: false, message: 'User not found' });
+  }
+
+  return res.json(userFound);
+};
