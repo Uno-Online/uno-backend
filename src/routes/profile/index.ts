48b5996f77a04cb0678107be090e5104fdca29f5
Alan Henrique <alancamargo50@gmail.com>
2023-03-23 11:49:25 -0300

    6 criar rota para retornar dados do usuário logado (#32)
    Co-authored-by: Alan Henrique <mac@Alans-MacBook-Air.local>

--- src/routes/profile/index.ts
+++ src/routes/profile/index.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { changeUsername } from './change-username';
+import { getUserLogged } from './get-user-logged';
 
 export const router = Router();
 
 router.put('/', changeUsername);
+router.get('/', getUserLogged);
