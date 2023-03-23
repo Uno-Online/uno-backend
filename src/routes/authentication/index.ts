e696b21a4ba978bc843b3f8962ed7f45fcfcd709
deyvis0n <deyvisonsferreira@outlook.com>
2023-03-23 15:09:39 -0300

    feat: user logout

--- src/routes/authentication/index.ts
+++ src/routes/authentication/index.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { register } from './register';
+import { logout } from './logout';
 
 export const router = Router();
 
 router.post('/register', register);
+router.put('/logout', logout)
---
+++ src/routes/authentication/logout.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express';
+import { CookieKey } from '../../constants/cookie-key';
+import { prisma } from '../../prisma';
+import { JwtService } from '../../services';
+
+export const logout = async (req: Request, res: Response) => {
+  const { [CookieKey.AuthToken]: authToken } = req.cookies;
+
+  try {
+    const payload = JwtService.decrypt<{ userId: number }>(authToken);
+
+    const userSession = await prisma.userSession.deleteMany({
+      where: {
+        userId: payload.userId,
+      },
+    });
+
+    if (userSession.count >= 1) {
+      res.clearCookie(CookieKey.AuthToken);
+      return res.json({ success: true });
+    }
+
+    return res
+      .status(400)
+      .json({ success: false, message: 'No user found with this token' });
+  } catch (err) {
+    return res.status(500).json({ success: false, message: 'Invalid JWT' });
+  }
+};
