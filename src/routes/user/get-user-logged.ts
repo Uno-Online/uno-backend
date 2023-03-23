4f4df386d3156f1bbcb9ff7d8dca31f44af6f974
Alan Henrique <alancamargo50@gmail.com>
2023-03-23 07:52:51 -0300

    Removido arquivo insomnia e ajustes no código para retornar usuário pelo req.user

--- src/routes/user/get-user-logged.ts
+++ src/routes/user/get-user-logged.ts
@@ -1,49 +1,18 @@
 import type { Response } from 'express';
 import { RequestWithUser } from '../../types/request-with-user';
-import { prisma } from '../../prisma';
-import { CookieKey } from '../../constants/cookie-key';
 
 /**
  * Retorna informações sobre o usuário logado
  * */
 export const getUserLogged = async (req: RequestWithUser, res: Response) => {
-  const userId = req.user?.id;
-  const token = req.cookies[CookieKey.AuthToken];
-
-  if (!userId || !token) {
+  if (!req.user) {
     return res.json({ success: false, message: 'user not logged' });
   }
 
-  const userSession = await prisma.userSession.findFirst({
-    where: {
-      AND: [
-        {
-          userId: Number(userId),
-        },
-        {
-          token,
-        },
-      ],
-    },
-  });
-
-  if (!userSession) {
-    return res.json({ success: false, message: 'error find session' });
-  }
-
-  const userLogged = await prisma.user.findUnique({
-    where: {
-      id: userSession.userId,
-    },
-    select: {
-      id: true,
-      username: true,
-      email: true,
-    },
-  });
-
-  if (!userLogged) {
-    return res.json({ success: false, message: 'user not found' });
+  const userLogged = {
+    id: req.user.id,
+    username: req.user.username,
+    email: req.user.email,
   }
 
   return res.json(userLogged);
