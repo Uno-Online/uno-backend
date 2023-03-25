cec13142c010c00e34e49f83d95efa34811e3432
Hélio Kröger <small.boat6245@heliokroger.com>
2023-03-25 03:56:38 +0000

    fix: adding status-code constant

--- src/constants/status-code.ts
+++ src/constants/status-code.ts
@@ -1,6 +1,8 @@
+/**
+ * Status de erro HTTP
+ * https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+ * */
 export enum StatusCode {
   BAD_REQUEST = 400,
   UNAUTHORIZED = 401,
-  NOT_FOUND = 404,
-  INTERNAL_SERVER_ERROR = 500,
 }
