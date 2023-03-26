4bdfdc7f4fafcd26ed2ba46eeb7ac1a72f7ee564
deyvis0n <82903398+deyvis0n@users.noreply.github.com>
2023-03-25 22:59:25 -0300

    feat: getRooms return room pagination on success (#58)

--- src/routes/rooms/get-rooms.ts
+++ src/routes/rooms/get-rooms.ts
@@ -1,16 +1,29 @@
 import { RoomState } from '@prisma/client';
 import { Response } from 'express';
+import { BadRequestException } from '../../exceptions';
 import { prisma } from '../../prisma';
 import type { RequestWithUser } from '../../types/request-with-user';
+import { paramsPaginationValidation } from './params-pagination.validation';
 
 /**
  * Retorna uma lista paginada de salas abertas
  * */
 export const getRooms = async (req: RequestWithUser, res: Response) => {
-  const { take, skip } = req.query;
+  const params = paramsPaginationValidation.safeParse(req.query);
 
-  res.json(
-    await prisma.room.findMany({
+  if (!params.success) {
+    throw new BadRequestException('Invalid param');
+  }
+
+  const [take, skip] = [Number(params.data.take), Number(params.data.skip)];
+
+  const [totalRooms, roomsResult] = await prisma.$transaction([
+    prisma.room.count({
+      where: {
+        state: RoomState.WAITING_FOR_PLAYERS,
+      },
+    }),
+    prisma.room.findMany({
       include: {
         _count: {
           select: { players: true },
@@ -19,8 +32,18 @@ export const getRooms = async (req: RequestWithUser, res: Response) => {
       where: {
         state: RoomState.WAITING_FOR_PLAYERS,
       },
-      take: Number(take ?? 50),
-      skip: Number(skip ?? 0),
-    })
-  );
+      take,
+      skip,
+    }),
+  ]);
+
+  const totalPage = totalRooms > 0 ? Math.ceil(totalRooms / take) : 1;
+  const currentPage = Math.floor(skip / take) + 1;
+
+  const rooms = roomsResult.map((obj) => {
+    const { _count: count, ...room } = obj;
+    return Object.assign(room, { players: count.players });
+  });
+
+  return res.json({ totalRooms, currentPage, totalPage, rooms });
 };
