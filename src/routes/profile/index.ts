48499a79501bed74488f9de6d1001770f87289ce
Douglinhas <daguiaralcantara@gmail.com>
2023-03-27 12:06:06 -0300

    feat: set updateAvatar controller to route '/'.

--- src/routes/profile/index.ts
+++ src/routes/profile/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { changeUsername } from './change-username';
 import { getUserLogged } from './get-user-logged';
+import { updateAvatar } from './update-avatar';
 
 export const router = Router();
 
+router.put('/', updateAvatar);
 router.put('/username/', changeUsername);
 router.get('/', getUserLogged);
