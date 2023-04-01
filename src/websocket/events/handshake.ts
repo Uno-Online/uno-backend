bc7dffd6d35b3f0569628a461ae98851a16b74ce
Kaolhou <xdroid42@gmail.com>
2023-03-31 22:15:17 -0300

    create join room event

--- src/websocket/events/handshake.ts
+++ src/websocket/events/handshake.ts
@@ -1,7 +1,23 @@
 import type { Server, Socket } from 'socket.io';
+import { JwtService } from '../../services';
+import { JwtPayload } from 'jsonwebtoken';
+import { prisma } from '../../prisma';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const handshake = (io: Server, socket: Socket) => {
-  // Valida o token do usuário `socket.handshake.auth.token`
-  // Desconecta caso o token seja inválido pelo `socket.disconnect()`
+export const handshake = async (io: Server, socket: Socket) => {
+  try {
+    const token = JwtService.decrypt(
+      socket.handshake.auth.token
+    ) as JwtPayload & { userId: string };
+    const count = await prisma.user.count({
+      where: {
+        id: Number(token.userId),
+      },
+    });
+
+    if (count === 0) throw new Error('no user found');
+
+    socket.data.userId = token.userId;
+  } catch (error) {
+    socket.disconnect();
+  }
 };
--- src/websocket/events/join-room.ts
+++ src/websocket/events/join-room.ts
@@ -1,11 +1,43 @@
 import type { Server, Socket } from 'socket.io';
+import { prisma } from '../../prisma';
 
 export function joinRoom(io: Server, socket: Socket) {
-  return async (roomId: number) => {
-    // Validação da sala e do user
-    const playerId = 1;
+  return async (roomId: string) => {
+    try {
+      // Validação da sala e do user
+      const id = parseInt(roomId);
+      const playerId = socket.data.userId;
+      const lastPlayer = await prisma.roomPlayer.findFirst({
+        where: {
+          roomId: id,
+        },
+        orderBy: {
+          index: 'desc',
+        },
+      });
 
-    socket.join(`room-${roomId}`);
-    io.to(`room-${roomId}`).emit('playerJoinedRoom', { playerId, roomId });
+      if (
+        !(await prisma.room.count({
+          where: {
+            state: 'WAITING_FOR_PLAYERS',
+            id,
+          },
+        }))
+      )
+        throw new Error();
+
+      await prisma.roomPlayer.create({
+        data: {
+          index: lastPlayer?.index || 0,
+          playerId,
+          roomId: id,
+        },
+      });
+
+      socket.join(`room-${roomId}`);
+      io.to(`room-${roomId}`).emit('playerJoinedRoom', { playerId, roomId });
+    } catch (error) {
+      return;
+    }
   };
 }
